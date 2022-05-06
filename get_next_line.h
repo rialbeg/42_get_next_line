de49abfba1cb5f190960824b29d3570479781ad8
rialbeg <gsamascarenhas@gmail.com>
2022-05-06 11:27:25 -0300

    first commit

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsilva-d <gsilva-d@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/05 21:59:43 by gsilva-d          #+#    #+#             */
+/*   Updated: 2022/05/05 22:05:48 by gsilva-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *left_str, char *buff);
+size_t	ft_strlen(char *s);
+
+#endif
---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "get_next_line.h"
+
+int main(void)
+{
+	FILE *file;
+	int i;
+	char *current_line;
+	char *file_path = "files/file1";
+	// char *file_path = "files/file2";
+	// char *file_path = "files/file3";
+	// char *file_path = "files/file4";
+	// char *file_path = "files/file5";
+
+	i = 0;
+	file = fopen(file_path, "r");
+
+	// _fileno gets fd in linux
+	while ((current_line = get_next_line(file->_fileno)) != NULL)
+	{
+		printf("Line %i : '%s", i, current_line);
+		free(current_line);
+		i++;
+	}
+
+
+	fclose(file);
+
+	return (0);
+}
